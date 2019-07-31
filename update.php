4df4499dde49d7bf3351b49482b6bb13236e3931
Mary De Nóbrega <denobrega.mary@gmail.com>
2019-07-31 19:49:43 +0200

    someone fails and it isn't me

---
+++ update.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use TelegramBot\Bot;
+use function json_decode;
+
+/**
+ * We receive a JSON-serialized Update {@see https://core.telegram.org/bots/api#update}
+ *
+ * @see https://github.com/unreal4u/telegram-api/blob/master/src/Telegram/Types/Update.php
+ * @see https://core.telegram.org/bots/api#getting-updates
+ * @see https://core.telegram.org/bots/api#update
+ */
+$update = file_get_contents('php://input');
+$input  = json_decode($update, true);
+
+try {
+    $bot = new Bot($input);
+    $bot->sendAnswer();
+} catch (\Throwable $e) {
+    // Log maybe?
+}
